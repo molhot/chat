dcc1d7f9d565d1bdc8e706d840879d45bb09d7a2
mol_hot <sakata19991214@gmail.com>
2022-03-14 04:21:40 +0900

    firstmake

---
+++ firstpage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+    <title>
+        チャットページ
+    </title>
+    <link rel="stylesheet" href="#">
+</head>
+<body>
+    <form action="firstpage.php" method="post">
+        <div class="insert_all">
+            <div class="insertform">
+                今の気持ちを一言！
+            </div>
+            <input type="text" name="insert">
+        </div>
+    </form>
+    <div class="bullet">
+
+    </div>
+</body>
+</html>
